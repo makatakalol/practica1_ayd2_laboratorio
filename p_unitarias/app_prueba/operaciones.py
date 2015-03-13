2cf8a1deb469813795874784b2132842f4f201e7
Alejandro <alejandro.calu@gmail.com>
2015-03-13 11:14:02 -0600

    proyecto con primeras pruebas

--- p_unitarias/app_prueba/operaciones.py
+++ p_unitarias/app_prueba/operaciones.py
@@ -1,6 +1,9 @@
 def suma(valor1,valor2):
-	total = valor1+valor2
-	return total
+	if validar_numero(valor1) == 0 and validar_numero(valor2) == 0:
+		total = valor1+valor2
+		return total
+	else:
+		return "error"
 
 def resta(valor1,valor2):
 	total = valor1-valor2
@@ -11,5 +14,20 @@ def multiplicacion(valor1,valor2):
 	return total
 
 def division(valor1,valor2):
-	total = valor1/valor2
-	return total
+	if valor2 == 0:
+		return "error"
+	else:
+		total = valor1/valor2
+		return total
+
+def validar_numero(val): #funcion para validar que el valor es numerico
+	valor = str(val)
+	largo = len(valor)
+	numeros = 0
+	nonumeros = 0
+	for x in range(largo):
+		if 47<ord(valor[x])<57 or ord(valor[x])==45:
+			numeros += 1
+		else:
+			nonumeros += 1
+	return nonumeros
--- p_unitarias/app_prueba/tests.py
+++ p_unitarias/app_prueba/tests.py
@@ -5,9 +5,11 @@ class pruebas_unitarias(TestCase):
 
 	def test_suma(self):
 		self.assertEquals(operaciones.suma(5,5),10)
+		self.assertEquals(operaciones.suma(50,30),80)
 	def test_resta(self):
 		self.assertEquals(operaciones.resta(5,5),0)
 	def test_multiplicacion(self):
 		self.assertEquals(operaciones.multiplicacion(5,5),25)
 	def test_division(self):
-		self.assertEquals(operaciones.division(5,5),1)
+		self.assertEquals(operaciones.division(5,0),"error")
+		self.assertEquals(operaciones.division(10,2),5)
